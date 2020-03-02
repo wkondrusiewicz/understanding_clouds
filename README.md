198904edece5668ffe7c06e19b0917a76e8fb472
Wiktor <vikkon00@gmail.com>
2020-03-02 16:23:16 +0100

    create structure of the repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Understanding clouds
+
+This project is an attempt to partake in [Kaggle Competition](https://www.kaggle.com/c/understanding_cloud_organization) as univeristy team project. Its main objective is to predict and classify masks of satelite cloud images.  
---
+++ setupy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(
+    name='understanding_clouds',
+    version='0.1',
+    description='Repository for predicting and classyfing masks for satelite cloud images',
+    packages=find_packages(include=['understanding_clouds']),
+    install_requires=[
+    ],
+)
